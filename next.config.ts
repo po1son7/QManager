d64ab960fd770786d2e95b63b116f6ac2af03266
Russel Yasol <russel.yasol@wvsu.edu.ph>
2026-04-23 15:23:28 +0800

    fix(manifest): update generated_at timestamp and add contributors for language packs

--- next.config.ts
+++ next.config.ts
@@ -7,16 +7,16 @@ const nextConfig: NextConfig = {
 
   // This block will be commented out before running bun run build and only used in development to proxy API requests to the modem's web server.
 
-  async rewrites() {
-    return [
-      {
-        source: "/cgi-bin/:path*",
-        //  cgi-bin path is used by the modem's web server for API requests, so we proxy all /api requests to the modem's IP address
-        destination: "http://192.168.224.1/cgi-bin/:path*",
-        basePath: false,
-      },
-    ];
-  },
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: "/cgi-bin/:path*",
+  //       //  cgi-bin path is used by the modem's web server for API requests, so we proxy all /api requests to the modem's IP address
+  //       destination: "http://192.168.224.1/cgi-bin/:path*",
+  //       basePath: false,
+  //     },
+  //   ];
+  // },
 };
 
 export default nextConfig;
